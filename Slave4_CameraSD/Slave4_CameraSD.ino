447352ff585d9a5fce83318b6b23624ef773bf21
Jason <crg.jason@gmail.com>
2016-08-10 11:23:57 -0400

    Update Slave4_CameraSD.ino

--- Slave4_CameraSD/Slave4_CameraSD.ino
+++ Slave4_CameraSD/Slave4_CameraSD.ino
@@ -1,17 +1,18 @@
+//Slave #4, responsible for the Camera and SD card (shield).
+//The code for this particular slave remains largely unfinished.
+
 #include <Wire.h>
 #include <SD.h>
 
 #define chipSelect 10
 
 void setup() {
-  // put your setup code here, to run once:
-  Wire.begin(6);
+  Wire.begin(6); //Receives event on channel 6
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
 
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
-    // don't do anything more:
     return;
   }
 }
@@ -20,8 +21,7 @@ File dataFile;
 String data = "";
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    //To be continued
 }
 
 void receiveEvent(int howMany){
@@ -31,37 +31,9 @@ void receiveEvent(int howMany){
     }
     dataFile = SD.open("testlog.txt", FILE_WRITE);
     dataFile.print(data);
-
-    /*switch (x) {
-       case '1':
-         dataFile = SD.open("lightlog.txt", FILE_WRITE);
-         dataFile.print(whateverreceived); Serial.println(" lux");
-         break;
-         
-       case '2':
-         dataFile = SD.open("templog.txt", FILE_WRITE);
-         dataFile.print(whateverreceived); Serial.println("˚ Fahrenheit.");
-         break;
-         
-       case '3':
-         data = "";
-         dataFile = SD.open("humidlog.txt", FILE_WRITE);
-         dataFile.print(whateverreceived); Serial.println(" %");
-         break;
-
-       case '4':
-         dataFile = SD.open("accellog.txt", FILE_WRITE);
-         dataFile.print(whateverreceived); Serial.println(" lux");
-         break;
-         
-       default:
-         Serial.println("Signal Ignored");
-         Serial.println("");
-  }
-  Wire.flush();*/
 }
 
 void getPicture() {
-  
+    //To be continued
 }
 
